71e5210fcbd04576821abc5f1670614b6648ca02
Enether <familyguyuser192@windowslive.com>
2017-03-05 13:53:40 +0200

    Rust RB Tree is a failed experiment, need to learn Borrowing before continuing

--- Trees/Rust/red_black_tree/src/main.rs
+++ Trees/Rust/red_black_tree/src/main.rs
@@ -1,3 +1,9 @@
+
+/*
+TODO: Learn Rust borrowing and then continue!
+*/
+
+use std::rc::Rc;
 #[derive(Debug)]
 #[derive(PartialEq)]
 enum Color {
@@ -10,9 +16,9 @@ enum Color {
 struct Node {
     value: Option<i32>,
     color: Color,
-    parent: Option<Box<Node>>,
-    left_child: Option<Box<Node>>,
-    right_child: Option<Box<Node>>,
+    parent: Option<Rc<Node>>,
+    left_child: Option<Rc<Node>>,
+    right_child: Option<Rc<Node>>,
 }
 
 
@@ -34,7 +40,7 @@ impl Node {
                     println!("{:?} is smaller than {:?}, so the new node goes to the right!", value, new_value);
                     match self.right_child {
                         Some(ref mut child) => {
-                            child.add_node(new_value);
+                            Rc::make_mut(child).unwrap().add_node(new_value);
                         }
                         _ => {panic!("Node with color {:?} and value {:?} should have a right child!", self.color, self.value);}
                     }
@@ -44,13 +50,59 @@ impl Node {
                 // create a new node
                 self.color = Color::RED;
                 self.value = Some(new_value);
-                self.left_child = Some(Box::new(LEAF));
-                self.right_child = Some(Box::new(LEAF));
+                self.left_child = Some(Rc::new(LEAF));
+                self.right_child = Some(Rc::new(LEAF));
                 // TODO: Check for reorder
+                
+                self.check_reorder();
+                
+            }
+        }
+    }
 
-                // if self.parent.unwrap().color == Color::RED {
-                //     // need to reorder
-                // }
+    fn change_color(&mut self, color: Color) {
+        self.color = color;
+    }
+
+    // fn recolor(&mut self, sibling: &Rc<Node>, grandfather: &Rc<Node>) {
+    //     sibling.color = Color::BLACK;
+    //     let mut parent = self.parent.as_mut().unwrap();
+    //     parent.change_color(Color::BLACK);
+    //     grandfather.color = Color::RED;
+    // }
+
+    // fn to_recolor(&mut self) -> bool {
+    //     /* Returns a boolean indicating if the node is valid for a recolor. */
+    //     let mut parent_is_left = false;
+    //     if let Some(grandfather) = self.parent.as_mut().unwrap().parent.as_mut() {
+    //         if grandfather.value.unwrap() > self.parent.unwrap().value.unwrap() {
+    //             parent_is_left = true;
+    //         }
+
+    //         if parent_is_left {
+    //             if let Some(sibling) = grandfather.right_child.as_ref() {
+    //                 if sibling.color == Color::RED{
+    //                     self.recolor(sibling, grandfather);
+    //                     return true;
+    //                 }
+    //             } else if let Some(sibling) = grandfather.left_child.as_ref() {
+    //                 if sibling.color == Color::RED {
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     // If no grandfather, return false
+    //     false
+    // }
+
+    fn check_reorder(&mut self) {
+        if let Some(parent) = self.parent.as_ref() {
+            if parent.color == Color::RED {
+                // need to reorder
+                // check for recolor
+                // determine parent - grandfather relationship    
+                
             }
         }
     }
@@ -76,7 +128,7 @@ mod tests {
     #[test]
     fn test_binary_tree_ordering() {
         /* Add a couple of nodes and assert that they are on the right place! */
-        let mut root = Node { value: Some(5), color: Color::RED, parent: None, left_child: Some(Box::new(LEAF)), right_child: Some(Box::new(LEAF))};
+        let mut root = Node { value: Some(5), color: Color::RED, parent: None, left_child: Some(Rc::new(LEAF)), right_child: Some(Rc::new(LEAF))};
         root.add_node(2);
         root.add_node(1);
         root.add_node(6);
